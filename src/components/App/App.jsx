bdd8743e3ba6c6deef90f170f4af652672826392
AH400796 <A.Hokhman@gmail.com>
2023-02-16 19:08:59 +0200

    up

---
+++ src/components/App/App.jsx
@@ -0,0 +1,57 @@
+import ContactList from 'components/ContactList';
+import ContactForm from 'components/ContactForm';
+import Filter from 'components/Filter';
+import { Wrapper, Title, ContactsWrapper } from './App.styled';
+
+// const initialsContacts = () =>
+//   JSON.parse(localStorage.getItem('saved_contacts')) ?? [];
+
+export default function App() {
+  // const [contacts, setContacts] = useState(initialsContacts);
+  // const [filterValue, setFilterValue] = useState('');
+
+  // useEffect(() => {
+  //   localStorage.setItem('saved_contacts', JSON.stringify(contacts));
+  // }, [contacts]);
+
+  // const formSubmitHandler = newContact =>
+  //   setContacts(prevState => [
+  //     ...prevState,
+  //     { id: nanoid(), name: newContact.name, number: newContact.number },
+  //   ]);
+
+  // const handleFilterInputChange = event => setFilterValue(event.target.value);
+
+  // const renderContacts = contacts
+  //   .filter(contact =>
+  //     contact.name.toLowerCase().includes(filterValue.toLowerCase())
+  //   )
+  //   .sort((a, b) => a.name.localeCompare(b.name));
+
+  // const deleteContact = id =>
+  //   setContacts(prevState => prevState.filter(contact => contact.id !== id));
+
+  // return (
+  //   <Wrapper>
+  //     <Title>Phonebook</Title>
+  //     <ContactForm onSubmitForm={formSubmitHandler} contacts={contacts} />
+  //     <Title>Contacts:</Title>
+  //     <ContactsWrapper>
+  //       <Filter filter={filterValue} onChange={handleFilterInputChange} />
+  //       <ContactList contacts={renderContacts} deleteContact={deleteContact} />
+  //     </ContactsWrapper>
+  //   </Wrapper>
+  // );
+
+  return (
+    <Wrapper>
+      <Title>Phonebook</Title>
+      <ContactForm />
+      <Title>Contacts:</Title>
+      <ContactsWrapper>
+        <Filter />
+        <ContactList />
+      </ContactsWrapper>
+    </Wrapper>
+  );
+}
--- src/components/Layout/Layout.jsx
+++
@@ -1,5 +0,0 @@
-import { Container } from './Layout.styled';
-
-export default function Layout({ children }) {
-  return <Container>{children}</Container>;
-};
